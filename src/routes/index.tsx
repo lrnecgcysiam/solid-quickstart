5e65254f1b6b8c201bdb560574c1dba4302c07ac
Chris Samuel <encryptable@pm.me>
2024-01-16 12:15:27 -0500

    updting routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,15 +4,15 @@ import Counter from "~/components/Counter";
 export default function Home() {
   return (
     <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
-      <Counter />
+      <Title>Classroom Activities</Title>
+      <h1>Classroom Activities</h1>
+      {/* <Counter /> */}
       <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
+        {/* Visit{" "} */}
+        {/* <a href="https://start.solidjs.com" target="_blank">
           start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
+        </a>{" "} */}
+        {/* to learn how to build SolidStart apps. */}
       </p>
     </main>
   );
